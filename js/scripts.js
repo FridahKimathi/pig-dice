31337677a4723d7828158fcd9494db9b4cb09446
fridah kim <fredakim18@gmail.com>
2018-12-15 17:51:58 +0300

    font style changed

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,32 @@
-//Business logic
-function pigDice(){
-    var die1=$("#die1").get();
-    var die2=$("#die2").get();
-    var points=("#points").get();
-    var r1=math.floor(math.random()*6)+1;
-    var r2=math.floor(math.random()*6)+1;
-    var diceTotal=r1+r2;
-    die1.innerHTML=r1;
-    die2.innerHTML=r2;
-    points.innerHTML="You just rolled" +diceTotal+"!!"
-        if(r1==1 || r2==1){
-            diceTotal=0;
-            points.innerHTML= "Your total" +diceTotal+"!!"
-        }
+
+$(document).ready(function () {
+    
+    var r1 = Math.floor(Math.random() * 6) + 1;
+    var r2 = Math.floor(Math.random()* 6) + 1;
+    var diceTotal = r1 + r2;
+
+    $("#die1").innerHTML = r1;
+    $("#die2").innerHTML= r2;
+    $("points").innerHTML = "You just rolled" + diceTotal + "!!"
+    if (r1 == 1 || r2 == 1) {
+        diceTotal = 0;
+        $("#points").innerHTML = "Your total" + diceTotal + "!!" + "Your turn is over"
+    } else if ("#points".innerHTML = "100") {
+         alert("GameOver!You won!")
     }
-    // User-interface logic
+});
     $(document).ready(function(){
+        var firstName=$(player1).val();
+        var secondName=$("player2").val();;
+        if(firstName ==0 || secondName ==0 ){
+            $(".noplayer").show();
+        }
         $("#start-btn").click(function(){
             $(".intro").hide();
-            $(".names").show
-            var firstPlayerName=$("#player1").val();
-            var secondPlayerName=$("#player2").val();
-            if(firstPlayerName.length==0 || secondPlayerName.length ==0){
-                $("no-player").show();
-            }else(){
-              $("names").hide();
-              $(".firstplayer").show();
-            }
+            $(".player1").show();
+
 
         })
-        
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
