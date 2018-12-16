2b9cc66f1d1a97c03d513ea6b034961f57e59179
fridah kim <fredakim18@gmail.com>
2018-12-16 23:44:10 +0300

    business logic changed to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
 //Business logic
 $(document).ready(function(){
-function player(name,roll,points){
-    this.name;
-    this.roll=0;
-    this.points=0;
-}
-player.prototype.Roll=function(){
+function player(name,roll,points,){
+this.name=name;
+this.roll=0;
+this.points=0;
+
     var die1=$("#die1").get();
     var die2=$("#die2").get();
     var points=$("#points").get();
     function rollDice(r1,r2){
-    var r1=new rollDice (math.floor(math.random()*6)+1);
-    var r2=new rollDice (math.floor(math.random()*6)+1);
+    var r1=new rollDice (Math.floor(Math.random()*6)+1);
+    var r2=new rollDice (Math.floor(Math.random()*6)+1);
+    }
+    this.roll=rollDice;
     rollDice.prototype.diceTotal=function(){var diceTotal=r1+r2;
     }
-}
+
    $ (die1).html()=r1;
     $(die2).html()=r2;
     $(points).html()="You just rolled" +diceTotal+"!!"
@@ -24,9 +25,9 @@ player.prototype.Roll=function(){
         }else if("diceTotal ==100"){
             $(points).html()="this.name"+"You win!!"
         }
+    
+
     }
-
-
 });
 
  // User-interface logic
@@ -34,8 +35,8 @@ player.prototype.Roll=function(){
     $("#start-btn").click(function(){
         $(".intro").hide();
         $(".player1",).show();
-        var firstPlayerName=$("#player1").val();
-        var secondPlayerName=$("#player2").val();
+        var firstPlayerName=$("#playerone").val();
+        var secondPlayerName=$("#playertwo").val();
         if(firstPlayerName.length==0 || secondPlayerName.length ==0){
             $("no-player").show();
         }else{
@@ -44,7 +45,7 @@ player.prototype.Roll=function(){
         })
 
         $(".roll").click(function(){
-            player.Roll=rollDice();
+            player.roll=rollDice();
             
         })
 
